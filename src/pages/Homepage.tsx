ebf529aee3f4d7a67b9bba6fea8542788abaee19
MdSakifHossain <sakif015sakif@gmail.com>
2026-02-25 22:35:28 +0600

    style(ui): improve homepage layout with svg and message

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,13 +1,11 @@
 // @ts-nocheck
-
 import MobileContent from "@/components/app/layouts/MobileContent";
-import { HugeiconsIcon } from "@hugeicons/react";
-import { Home07Icon } from "@hugeicons/core-free-icons";
 
 export default function Homepage() {
   return (
     <MobileContent className="items-center! justify-center!">
-      <HugeiconsIcon icon={Home07Icon} strokeWidth={1} className="size-16" />
+      <img className="w-10/12" src="/homepage-img.svg" alt="Greetings" />
+      <p className="text-2xl">New Features Coming!</p>
     </MobileContent>
   );
 }
