4ed8b469bafc81befefe75dd186d86bb4c428c65
MdSakifHossain <sakif015sakif@gmail.com>
2026-03-02 10:43:55 +0600

    feat(salah_intentions): add niyats for first 3 prayers

--- src/constants/salah_intentions.ts
+++ src/constants/salah_intentions.ts
@@ -1,8 +1,8 @@
 export const salah_names = ["fajr", "zuhr", "asr", "maghrib", "isha"];
 export const salah_intentions = {
-  fojor: [
+  fajr: [
     {
-      title: "2 Rakah Sunnat",
+      title: `2 rakath sunnath`,
       apt: {
         arabic: `نَوَايْتُ اَنْ اُصَلِّىَ لِلَّهِ تَعَالَى رَكْعَتَىْ صَلَوةِ الْفَجْرِ سُنَّةُ رَسُوْلُ اللهِ تَعَالَى مُتَوَجِّهًا اِلَى جِهَةِ الْكَعْبَةِ الشَّرِيْفَةِ اَللهُ اَكْبَرُ`,
         pronunciation: `Nawai tuan usallia lillahi ta'ala rak'atai swalathil Fajre sunnathu rasulullahi ta'ala muthawaj-jihan ila jihatil ka'batish sharifathi, Allahu akbar`,
@@ -10,7 +10,7 @@ export const salah_intentions = {
       },
     },
     {
-      title: "2 Rakah Farz",
+      title: `2 rakath fard`,
       apt: {
         arabic: `نَوَايْتُ اَنْ اُصَلِّىَ لِلَّهِ تَعَالَى رَكْعَتَىْ صَلَوةِ الْفَجْرِفَرْضُ اللهِ تَعَالَى مُتَوَجِّهًا اِلَى جِهَةِ الْكَعْبَةِ الشَّرِيْفَةِ اَللهُ اَكْبَرُ`,
         pronunciation: `Nawai tuan usallia lillahi ta'ala rak'atai swalathil Fajre fardhullahi ta'ala muthawaj-jihan ila jihatil ka'batish sharifathi, Allahu akbar`,
@@ -18,13 +18,131 @@ export const salah_intentions = {
       },
     },
   ],
-  zohor: [
+  zuhr: [
     {
-      title: "2 Rakah Farz",
+      title: `4 rakath sunnath`,
       apt: {
-        arabic: `نَوَايْتُ اَنْ اُصَلِّىَ لِلَّهِ تَعَالَى رَكْعَتَىْ صَلَوةِ الْفَجْرِفَرْضُ اللهِ تَعَالَى مُتَوَجِّهًا اِلَى جِهَةِ الْكَعْبَةِ الشَّرِيْفَةِ اَللهُ اَكْبَرُ`,
-        pronunciation: `Nawai tuan usallia lillahi ta'ala rak'atai swalathil Fajre fardhullahi ta'ala muthawaj-jihan ila jihatil ka'batish sharifathi, Allahu akbar`,
-        translation: `I turn to Allah the most high with the intention to pray 2 unit Fajr that is obligatory to do for the most high facing towards the noble Ka'bah, Allah is most great`,
+        arabic: `نَوَايْتُ اَنْ اُصَلِّىَ لِلَّهِ تَعَالَى اَرْبَعَ رَكَعَاتِ صَلَوةِ الْظُهْرِ سُنَّةُ رَسُوْلُ اللهِ تَعَالَى مُتَوَجِّهًا اِلَى جِهَةِ الْكَعْبَةِ الشَّرِيْفَةِ اَللهُ اَكْبَرُ`,
+        pronunciation: `Nawai tuan usallia lillahi ta'ala arba'a rak'athi swalathil Zuhre sunnathu rasulullahi ta'ala muthawaj-jihan ila jihatil ka'batish sharifathi, Allahu akbar`,
+        translation: `I turn to Allah the most high with the intention to pray 4 unit Zuhr the way the messenger of Allah did for the most high facing towards the noble Ka'bah, Allah is most great`,
+      },
+    },
+    {
+      title: `4 rakath fard`,
+      apt: {
+        arabic: `نَوَايْتُ اَنْ اُصَلِّىَ لِلَّهِ تَعَالَى اَرْبَعَ رَكَعَاتِ صَلَوةِ الْظُهْرِ فَرْضُ اللهِ تَعَالَى مُتَوَجِّهًا اِلَى جِهَةِ الْكَعْبَةِ الشَّرِيْفَةِ اَللهُ اَكْبَرُ`,
+        pronunciation: `Nawai tuan usallia lillahi ta'ala arba'a rak'athi swalathil Zuhre fardhullahi ta'ala muthawaj-jihan ila jihatil ka'batish sharifathi, Allahu akbar`,
+        translation: `I turn to Allah the most high with the intention to pray 4 unit Zuhr that is obligatory to do for the most high facing towards the noble Ka'bah, Allah is most great`,
+      },
+    },
+    {
+      title: `2 rakath sunnath`,
+      apt: {
+        arabic: `نَوَايْتُ اَنْ اُصَلِّىَ لِلَّهِ تَعَالَى رَكْعَتَىْ صَلَوةِ الْظُهْرِ سُنَّةُ رَسُوْلُ اللهِ تَعَالَى مُتَوَجِّهًا اِلَى جِهَةِ الْكَعْبَةِ الشَّرِيْفَةِ اَللهُ اَكْبَرُ`,
+        pronunciation: `Nawai tuan usallia lillahi ta'ala rak'atai swalathil Zuhre sunnathu rasulullahi ta'ala muthawaj-jihan ila jihatil ka'batish sharifathi, Allahu akbar`,
+        translation: `I turn to Allah the most high with the intention to pray 2 unit Zuhr the way the messenger of Allah did for the most high facing towards the noble Ka'bah, Allah is most great`,
+      },
+    },
+    {
+      title: `2 rakath nafl`,
+      apt: {
+        arabic: `نَوَايْتُ اَنْ اُصَلِّىَ لِلَّهِ تَعَالَى رَكْعَتَىْ صَلَوةِ الْظُهْرِ نَفْلِ مُتَوَجِّهًا اِلَى جِهَةِ الْكَعْبَةِ الشَّرِيْفَةِ اَللهُ اَكْبَرُ`,
+        pronunciation: `Nawai tuan usallia lillahi ta'ala rak'atai swalathil Zuhre nafli muthawaj-jihan ila jihatil ka'batish sharifathi, Allahu akbar`,
+        translation: `I turn to Allah the most high with the intention to pray 2 unit Zuhr voluntarily facing towards the noble Ka'bah, Allah is most great`,
+      },
+    },
+  ],
+  asr: [
+    {
+      title: `4 rakath sunnath`,
+      apt: {
+        arabic: `نَوَايْتُ اَنْ اُصَلِّىَ لِلَّهِ تَعَالَى اَرْبَعَ رَكَعَاتِ صَلَوةِ الْعَصْرِ سُنَّةُ رَسُوْلُ اللهِ تَعَالَى مُتَوَجِّهًا اِلَى جِهَةِ الْكَعْبَةِ الشَّرِيْفَةِ اَللهُ اَكْبَرُ`,
+        pronunciation: `Nawai tuan usallia lillahi ta'ala arba'a rak'athi swalathil A'sre sunnathu rasulullahi ta'ala muthawaj-jihan ila jihatil ka'batish sharifathi, Allahu akbar`,
+        translation: `I turn to Allah the most high with the intention to pray 4 unit A'sr the way the messenger of Allah did for the most high facing towards the noble Ka'bah, Allah is most great`,
+      },
+    },
+    {
+      title: `4 rakath fard `,
+      apt: {
+        arabic: `نَوَايْتُ اَنْ اُصَلِّىَ لِلَّهِ تَعَالَى اَرْبَعَ رَكَعَاتِ صَلَوةِ الْعَصْرِ فَرْضُ اللهِ تَعَالَى مُتَوَجِّهًا اِلَى جِهَةِ الْكَعْبَةِ الشَّرِيْفَةِ اَللهُ اَكْبَرُ`,
+        pronunciation: `Nawai tuan usallia lillahi ta'ala arba'a rak'athi swalathil A'sre fardhullahi ta'ala muthawaj-jihan ila jihatil ka'batish sharifathi, Allahu akbar`,
+        translation: `I turn to Allah the most high with the intention to pray 4 unit A'sr that is obligatory to do for the most high facing towards the noble Ka'bah, Allah is most great`,
+      },
+    },
+  ],
+  maghrib: [
+    {
+      title: ``,
+      apt: {
+        arabic: ``,
+        pronunciation: ``,
+        translation: ``,
+      },
+    },
+    {
+      title: ``,
+      apt: {
+        arabic: ``,
+        pronunciation: ``,
+        translation: ``,
+      },
+    },
+    {
+      title: ``,
+      apt: {
+        arabic: ``,
+        pronunciation: ``,
+        translation: ``,
+      },
+    },
+  ],
+  isha: [
+    {
+      title: ``,
+      apt: {
+        arabic: ``,
+        pronunciation: ``,
+        translation: ``,
+      },
+    },
+    {
+      title: ``,
+      apt: {
+        arabic: ``,
+        pronunciation: ``,
+        translation: ``,
+      },
+    },
+    {
+      title: ``,
+      apt: {
+        arabic: ``,
+        pronunciation: ``,
+        translation: ``,
+      },
+    },
+    {
+      title: ``,
+      apt: {
+        arabic: ``,
+        pronunciation: ``,
+        translation: ``,
+      },
+    },
+    {
+      title: ``,
+      apt: {
+        arabic: ``,
+        pronunciation: ``,
+        translation: ``,
+      },
+    },
+    {
+      title: ``,
+      apt: {
+        arabic: ``,
+        pronunciation: ``,
+        translation: ``,
       },
     },
   ],
